2717ba55f5b748857d79fc000c3341275537af34
kamhar-ngado <kamhar.ngado.id@gmail.com>
2022-05-28 21:11:56 +0800

    add files

---
+++ addTwoNumbers.js
@@ -0,0 +1,7 @@
+let l1 = [2, 4, 3];
+let l2 = [5, 6, 4];
+let output = 807;
+
+const addTwoNumbers = (l1, l2) => {
+  let indexes = [];
+};
---
+++ loop.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 2; i++) {
+  console.log("loop luar ke -", i);
+  for (let j = 0; j < 2; j++) {
+    console.log("loop dalam ke -", j);
+  }
+  // console.log("ini loop luar, looping ke -", i);
+}
